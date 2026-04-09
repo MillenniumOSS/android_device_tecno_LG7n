97bfbf202fa3fcf3c8e3c023df19b30f6baea433
Shirayuki39 <lorddemecrius83@proton.me>
2026-04-10 01:52:27 +0800

    LG7n: Update flags
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- lineage_LG7n.mk
+++ lineage_LG7n.mk
@@ -42,3 +42,17 @@ PERF_ANIM_OVERRIDE := true
 # CPU governor support
 PERF_GOV_SUPPORTED := true
 PERF_DEFAULT_GOV := schedutil
+
+# AxionFX
+TARGET_INCLUDE_AXFX := true
+
+# GMS
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+PRODUCT_PACKAGES += \
+   LatinIMEGooglePrebuilt \
+   CalendarGooglePrebuilt \
+   GoogleContacts \
+   GoogleDialer \
+   PrebuiltBugle \
+   com.google.android.dialer.support
